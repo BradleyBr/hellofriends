a4538cde793046a9e1fc1597618efb7330008efc
Brad <br.brooks22@gmail.com>
2018-02-05 20:41:07 -0800

    Added html and css for index page, slick css/js folder

---
+++ resources/js/main.js
@@ -0,0 +1,13 @@
+$(document).ready(function() {
+  $('.carousel').slick({
+    autoplay: false,
+    autoplaySpeed: 7000,
+    infinite: true,
+    dots: true,
+    arrows: false,
+    draggable: false,
+    pauseOnFocus: false,
+    pauseOnHover: false,
+    adaptiveWidth: true
+  });
+})
